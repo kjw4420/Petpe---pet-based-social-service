64000b7b2b6483ff85be5eda27bf9ef4ecbffd9a
jobkaehenry <1zzangjun@gmail.com>
2022-08-02 23:26:49 +0900

    stoy changed

--- src/pages/Story.js
+++ src/pages/Story.js
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { StoryWrapper, UserProfile } from "../components/globalComponent";
 
-const Story=()=>{
-  return(
-    
-  )
+const Story = () => {
+  return (
+    <>
+      <h1>stroy</h1>
+      <h2>stroy</h2>
+    </>
+  );
 };
 
 export default Story;
