1622176a73ca3447954966383469d87d8107f2f8
suyeong jang <jangsuyeong923@naver.com>
2022-08-07 17:48:39 +0900

    place added

--- src/pages/place.js
+++ src/pages/place.js
@@ -4,23 +4,93 @@ import "./Place.css";
 
 
 const Place = () => {
-  return(
-  <>
-
-      <div className="place_header">
-        <div  className="place_search">
+  return (
+    <>
+      <div className="place_wrapper">
+        <div className="search_box">
           <input type="text" className="search" placeholder=" 검색" />
         </div>
+
         <div className="category">
-          <MiniButton onClick={(e)=>{e.target.classList.toggle("active")}} className = "active">전체보기</MiniButton>
-          <MiniButton onClick={(e)=>{e.target.classList.toggle("active")}}>레스토랑</MiniButton>
-          <MiniButton onClick={(e)=>{e.target.classList.toggle("active")}}>카페</MiniButton>
-          <MiniButton onClick={(e)=>{e.target.classList.toggle("active")}}>호텔</MiniButton>
+          <MiniButton
+            onClick={(e) => {
+              e.target.classList.toggle("active");
+            }}
+            className="active"
+          >
+            전체보기
+          </MiniButton>
+          <MiniButton
+            onClick={(e) => {
+              e.target.classList.toggle("active");
+            }}
+          >
+            레스토랑
+          </MiniButton>
+          <MiniButton
+            onClick={(e) => {
+              e.target.classList.toggle("active");
+            }}
+          >
+            카페
+          </MiniButton>
+          <MiniButton
+            onClick={(e) => {
+              e.target.classList.toggle("active");
+            }}
+          >
+            호텔
+          </MiniButton>
+        </div>
+        <div className="arrow_array">
+          <span className="array">정확도순/ 거리순</span>
+          <div className="arrow_btn">
+            <img src="./img/arrow.png" alt="정렬"></img>
+          </div>
+        </div>
+        <PlaceElement></PlaceElement>
+        <PlaceElement></PlaceElement>
+        <PlaceElement></PlaceElement>
+        <PlaceElement></PlaceElement>
+        <PlaceElement></PlaceElement>
       </div>
-        
-      </div>
-  </>  
-  )
+    </>
+  );
 };
+
+export const PlaceElement = () => {
+  return (
+    <>
+      <div className="info">
+        <img
+          src="http://placeimg.com/100/100/people"
+          alt="/이미지"
+          className="info_profile"
+        />
+        <div className="place_info">
+            <span className="place_name h2">댕댕병원</span>
+            <div className="place_grade">
+              <div className="grade">
+                <img src="./img/rate.png" alt="평점" className="rate_img"></img>
+                <span className="rate">4.23</span> 
+                <span className="time">09:00 ~ 18:00 (일 휴무)</span>
+                <span className="parking">주차가능</span>
+              </div>
+            </div>
+            <div className="location">
+              <span className="how_locatain">
+                부평구청 뒤 해밀턴 호텔에서 300미터 직진
+              </span>
+            </div>
+          </div>
+      </div>
+    </>
+  );
+};
+
+
+
+
+
 
 export default Place;
