8801da575bbdf1e6ddbe05d5c4c33a31de34ec06
jobkaehenry <1zzangjun@gmail.com>
2022-08-11 23:53:55 +0900

    app

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,68 @@
 import "./Login.css";
-import React, { useState } from "react";
+import React, { useState, useRef,useEffect } from "react";
 import Container from "../components/container";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import ButtonLarge from "../components/globalComponent";
-import useAuth from './../hooks/useAuth';
+import useAuth from "./../hooks/useAuth";
+import axios from "../../node_modules/axios/index";
 
-function Login() {
-  const {setAuth} = useAuth()
-  const [idValue, setIdInput] = useState(false);
-  const [pwValue, setPwInput] = useState(false);
-  const errorAlerterRef = React.useRef()
+const Login = () => {
+  const { setAuth } = useAuth();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  const [user, setUser] = useState(false);
+  const [pwd, setPwd] = useState(false);
+  const [errMsg, setErrMsg] = useState('');
+  const errRef = useRef();
+
+  const errorAlerterRef = useRef();
+
+  const userIdRef = useRef();
+  useEffect(() => {
+    userIdRef.current.focus();
+  }, []);
+
+
+  useEffect(() => {
+    setErrMsg('');
+}, [user, pwd])
+
+  // 로그인 제출 함수
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+        const response = await axios.post("http://3.39.181.250/accounts/login/",
+            JSON.stringify({ user, pwd }),
+            {
+                headers: { 'Content-Type': 'application/json' },
+                withCredentials: true
+            }
+        );
+        console.log(JSON.stringify(response?.data));
+        console.log(JSON.stringify(response));
+        const accessToken = response?.data?.accessToken;
+        const roles = response?.data?.roles;
+        setAuth({ user, pwd, roles, accessToken });
+        setUser('');
+        setPwd('');
+        navigate(from, { replace: true });
+    } catch (err) {
+        if (!err?.response) {
+            setErrMsg('No Server Response');
+        } else if (err.response?.status === 400) {
+            setErrMsg('Missing Username or Password');
+        } else if (err.response?.status === 401) {
+            setErrMsg('Unauthorized');
+        } else {
+            setErrMsg('Login Failed');
+        }
+        errRef.current.focus();
+    }
+}
 
 
   // 유효성검사함수
@@ -19,11 +72,10 @@ function Login() {
     // 형식에 맞는 경우 true 리턴
     if (email.test(e.target.value)) {
       errorAlerterRef.current.textContent = "";
-      setIdInput(true);
+      setUser(true);
     } else {
-      errorAlerterRef.current.textContent =
-        "올바른 이메일을 입력해주세요";
-      setIdInput(false);
+      errorAlerterRef.current.textContent = "올바른 이메일을 입력해주세요";
+      setUser(false);
     }
   };
 
@@ -36,13 +88,12 @@ function Login() {
       errorAlerterRef.current.textContent = "";
 
       //비번이 맞을시, pw검사 state를 true로
-      setPwInput(true);
+      setPwd(true);
     } else {
-      errorAlerterRef.current.textContent =
-        "8자리 이상의 영문, 숫자, 특수문자";
+      errorAlerterRef.current.textContent = "8자리 이상의 영문, 숫자, 특수문자";
 
       //비번이 틀릴시, pw검사 state를 false로
-      setPwInput(false);
+      setPwd(false);
     }
   };
 
@@ -51,21 +102,22 @@ function Login() {
       <div className="login_Wrapper">
         <header>
           <Link to={"/"}>
-          <h1>
-            <img src="./img/logo.png" alt="petpe_logo" />
-          </h1>
+            <h1>
+              <img src="./img/logo.png" alt="petpe_logo" />
+            </h1>
           </Link>
         </header>
 
         <form method="post" action="#">
           <div className="input_wrapper">
             <input
+              ref={userIdRef}
               type="text"
               name="email"
               id="email"
               placeholder="이메일주소를 입력해 주세요"
+              autoComplete="username"
               onChange={handleId}
-
             />
           </div>
           <div className="input_wrapper">
@@ -74,8 +126,8 @@ function Login() {
               name="password"
               id="password"
               placeholder="비밀번호를 입력해주세요"
+              autoComplete="current-password"
               onChange={handlePw}
-
             />
           </div>
           <div className="LoginOptionWrap">
@@ -98,24 +150,24 @@ function Login() {
               </span>
             </div>
           </div>
+          <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
           <div id="errorAlerter" ref={errorAlerterRef}></div>
+          
 
           {/* 조건부 링크 */}
-          <Link to={idValue && pwValue ? "/" : "#" }>
+
             <ButtonLarge
               as="input"
               type="submit"
-              className={idValue && pwValue ? "" : "disabled"}
-              value = "로그인"
-              // onClick={onLogin(idValue,pwValue)}
-            >
-            </ButtonLarge>
+              className={user && pwd ? "" : "disabled"}
+              value="로그인"
+              onClick={handleSubmit}
+            ></ButtonLarge>
 
-          </Link>
         </form>
       </div>
-      </Container>
+    </Container>
   );
-}
+};
 
 export default Login;
