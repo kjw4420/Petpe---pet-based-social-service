f603bc7ac348b4bcda50a80d19ec48f353680e53
jobkaehenry <1zzangjun@gmail.com>
2022-08-12 21:49:50 +0900

    login permission added

---
+++ src/api/axios.js
@@ -0,0 +1,12 @@
+import axios from "../../node_modules/axios/index";
+const BASE_URL = 'http://3.39.181.250';
+
+export default axios.create({
+    baseURL: BASE_URL
+});
+
+export const axiosPrivate = axios.create({
+    baseURL: BASE_URL,
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true
+});
