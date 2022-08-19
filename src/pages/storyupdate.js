403ea770365dd4ba9b8054884b19eabbc1b5ad8f
suyeong jang <jangsuyeong923@naver.com>
2022-08-20 00:30:19 +0900

    asdsad

--- src/pages/storyupdate.js
+++ src/pages/storyupdate.js
@@ -6,58 +6,69 @@ import TopHeader from "./../components/TopHeader";
 import MiniButton from "./../components/minibutton";
 import { MobileWrapper } from "./../components/globalComponent";
 import useAuth from "./../hooks/useAuth";
-import { stringify } from "../../../../../../AppData/Local/Microsoft/TypeScript/4.7/node_modules/jest-matcher-utils/build/index";
 
 
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'x-CSRFToken';
 
 const NewStory = () => {
-
-  const [contents, setContents]=useState();
-  const [picture, setPicture]=useState();
-
-  const onSubmit = async (e) => {
+  const [contents, setContents] = useState();
+  const [Picture, setPicture]=useState();
+  const { auth } = useAuth();
+  
+  const handleSubmit = (e) => {
     e.preventDefault();
-    e.persist();
-
-    let files = e.target.profile_files.files;
-    let formData = new FormData();
-    for (let i = 0; i < files.length; i++) {
-      formData.append("files", files[i]);
+    const formData = new FormData();
+    
+    formData.append("picture ", Picture);
+    formData.append("contents", contents);
+    console.log(formData);
+    
+    try {
+      axios
+        .post(
+          "http://3.34.21.153/story/",formData, 
+          {
+            headers: {
+              "content-type": "multipart/form-data",
+              Authorization: `Bearer ${auth.accessToken}`,
+            },
+            withCredentials: true,
+            "Access-Control-Allow-Credentials": "*",
+          }
+        )
+        .then((response) => {
+          console.log(response);
+        });
+    } catch (err) {
+      console.log(err);
     }
-
-    let dataSet = {
-      title: "",
-      contents: contents,
-      picture: picture,
-    };
-
-    formData.append("data", JSON.stringify(dataSet));
-
-    const postSurvey = await axios({
-      method: "POST",
-      url: "http://3.34.21.153/story/",
-      mode: "cors",
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-      data: formData,
-    });
-
-    console.log(postSurvey);
   };
 
-  return (
-    <form onSubmit={(e) => onSubmit(e)}>
-      <input
-        type="file"
-        name="profile_files"
-        multiple="multiple"
-      />
 
-      <button type="submit">제출</button>
-    </form>
+  return (
+    <>
+      <TopHeader
+        type="3"
+        name="새 스토리"
+        callBackType="text"
+        callBackText="저장"
+      />
+      <MobileWrapper>
+        <form>
+          <textarea
+            placeholder="내용을 입력하세요"
+            onChange={(e) => setContents(e.target.value)}
+          />
+          <input 
+          type="file" 
+          onChange={(e)=> setPicture(e.target.files)}/> 
+          
+          <MiniButton onClick={handleSubmit}>제출</MiniButton>
+        </form>
+      </MobileWrapper>
+    </>
   );
 };
+
 export default NewStory;
